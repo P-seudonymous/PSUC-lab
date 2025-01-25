7c3ffd6f60e928688c8e8699fe1acd4b1ddb8b18
P-seudonymous <aryan.sarafhp@gmail.com>
2025-01-25 15:33:08 +0530

    lab_2

---
+++ lab2/quadratic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double a, b, c, D, x1, x2;
+    printf("enter num: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    D = pow(b,2) - 4 *a*c;
+
+
+
+}
